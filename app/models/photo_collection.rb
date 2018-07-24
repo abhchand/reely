29bd01741ece6f5966e3b31f5c15dbe51fa22c4a
Abhishek Chandrasekhar <me@abhchand.me>
2018-07-23 22:58:44 -0400

    Adding `before_save` hook to validate that `photo` and `collection` have the same owner

--- app/models/photo_collection.rb
+++ app/models/photo_collection.rb
@@ -5,4 +5,17 @@ class PhotoCollection < ActiveRecord::Base
   validates :photo, presence: true
   validates :collection, presence: true
   validates :collection_id, uniqueness: { scope: :photo_id }
+
+  before_save :validate_owners_match
+
+  private
+
+  def validate_owners_match
+    return if photo.owner_id == collection.owner_id
+    errors.add(
+      :base,
+      "Photo owner #{photo.owner_id} does not match Collection "\
+        "owner #{collection.owner_id}"
+    )
+  end
 end
--- spec/models/photo_collection_spec.rb
+++ spec/models/photo_collection_spec.rb
@@ -1,7 +1,10 @@
 require "rails_helper"
 
 RSpec.describe PhotoCollection, type: :model do
-  subject { create(:photo_collection) }
+  let(:photo) { create(:photo) }
+  let(:collection) { create(:collection, owner: photo.owner) }
+
+  subject { create(:photo_collection, photo: photo, collection: collection) }
 
   describe "Associations" do
     it { should belong_to(:photo) }
@@ -12,5 +15,20 @@ RSpec.describe PhotoCollection, type: :model do
     it { should validate_presence_of(:photo) }
     it { should validate_presence_of(:collection) }
     it { should validate_uniqueness_of(:collection_id).scoped_to(:photo_id) }
+  end
+
+  describe "Callbacks" do
+    describe "before_save" do
+      describe "#validate_owners_match" do
+        it "validates the photo and collection owners are the same" do
+          photo
+          collection
+
+          photo.update!(owner: create(:user))
+
+          expect(subject.errors[:base].first).to match(/Photo owner.*does not/)
+        end
+      end
+    end
   end
 end
