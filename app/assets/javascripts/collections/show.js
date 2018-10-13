9064d1e402940ee932c035dbd8a9e4d11d90e496
Abhishek Chandrasekhar <me@abhchand.me>
2018-10-13 14:46:58 -0400

    Consolidating shared modal code into new common file function `openCollectionsDeleteModal()`

--- app/assets/javascripts/collections/show.js
+++ app/assets/javascripts/collections/show.js
@@ -3,18 +3,6 @@ $(document).ready(function() {
     var dataId = $(".collections-show__action-bar").attr("data-id");
     var dataName = $("#collection_name").val();
 
-    //
-    // TODO: Move this to a common "open modal" function
-    //
-
-    // Add data-id to modal
-    $(".modal").attr("data-id", dataId);
-
-    // Set modal heading
-    var heading = I18n.t("collections.delete_modal.heading", { collection_name: dataName });
-    $(".modal .modal-content__heading").html(heading);
-
-    // Open modal
-    $(".modal").addClass("active");
+    openCollectionsDeleteModal(dataId, dataName);
   });
 });
---
+++ app/assets/javascripts/common.js
@@ -0,0 +1,11 @@
+function openCollectionsDeleteModal(dataId, dataName) {
+// Add data-id to modal
+$(".modal").attr("data-id", dataId);
+
+// Set modal heading
+var heading = I18n.t("collections.delete_modal.heading", { collection_name: dataName });
+$(".modal .modal-content__heading").html(heading);
+
+// Open modal
+$(".modal").addClass("active");
+};
