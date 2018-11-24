f4b7344516b7e74a1b096218bddd608cffeb28ae
Abhishek Chandrasekhar <me@abhchand.me>
2018-11-24 01:37:35 -0500

    Setting up githooks and adding git pre-commit hooks

---
+++ contrib/git/hooks/scripts/stylelint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#
+# Pre-commit hook to run stylelint on files being staged
+#
+
+echo "=== Running stylelint..."
+
+STYLELINT="node_modules/.bin/stylelint"
+FILES=()
+
+if [[ ! -x "$STYLELINT" ]]; then
+  echo "\t\033[41mPlease install stylelint\033[0m (yarn install)"
+  exit 1
+fi
+
+for file in $(git diff --diff-filter=d --cached --name-only | grep -E '\.(css|scss|sass)$')
+do
+  # we only want to lint the staged changes, not any un-staged changes
+  git show ":$file" | "$STYLELINT" --stdin --stdin-filename "$file"
+  if [ $? -ne 0 ]; then
+    FILES=("${FILES[@]}" "$file")
+  fi
+done
+
+if [ ${#FILES[@]} -ne 0 ]; then
+  echo "\033[0;31mStylelint failed for ${#FILES[@]} files.\033[0m
+Please check your code and try again. You can run StyleLint manually via '$ node_modules/.bin/stylelint app/assets/stylesheets/<directory|file>'"
+  exit 1
+elif [ ${#FILES[@]} -eq 0 ]; then
+  echo "Stylelint completed with no warnings or errors."
+fi
