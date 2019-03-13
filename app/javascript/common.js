0ab5541d77323f339f629cf3cc4976c9528b8602
Abhishek Chandrasekhar <me@abhchand.me>
2019-03-12 23:19:13 -0400

    Eslint autofix

--- app/javascript/common.js
+++ app/javascript/common.js
@@ -7,11 +7,11 @@ function openCollectionsDeleteModal(dataId, dataName) {
   $(".modal").attr("data-id", dataId);
 
   // Set modal heading
-  var heading = I18n.t("collections.delete_modal.heading", { collection_name: dataName });
+  const heading = I18n.t("collections.delete_modal.heading", { collection_name: dataName });
   $(".modal .modal-content__heading").html(heading);
 
   // Open modal
   $(".modal").addClass("active");
-};
+}
 
 export {openCollectionsDeleteModal};
