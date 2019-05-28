51ef2bfe6a19fdba44c003e520df36b7d5d4f0e9
Abhishek Chandrasekhar <me@abhchand.me>
2019-05-28 16:16:06 -0400

    Adding `IconCollection` and `IconPlus`

--- app/javascript/components/icons.jsx
+++ app/javascript/components/icons.jsx
@@ -1,5 +1,44 @@
+/* eslint max-len: "off" */
+
 import PropTypes from "prop-types";
 import React from "react";
+
+class IconCollection extends React.Component {
+  static propTypes = {
+    size: PropTypes.string,
+    fillColor: PropTypes.string
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      size: "32",
+      fillColor: "#EEEEEE"
+    };
+  }
+
+  render() {
+    return (
+      <svg width={this.props.size} height={this.props.size} viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <g id="Artboard" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+          <g id="collection" transform="translate(5.000000, -0.000000)" stroke={this.props.fillColor} strokeWidth="1.06382979">
+            <g id="collection-2" transform="translate(6.000000, 4.000000)">
+              <path d="M79.5319149,0.531914894 L79.5319149,89.8723404 C79.5319149,91.3411828 78.3411828,92.5319149 76.8723404,92.5319149 L0.531914894,92.5319149 L0.531914894,93.8723404 C0.531914894,94.7536459 1.24635412,95.4680851 2.12765957,95.4680851 L82.8723404,95.4680851 C83.7536459,95.4680851 84.4680851,94.7536459 84.4680851,93.8723404 L84.4680851,2.12765957 C84.4680851,1.24635412 83.7536459,0.531914894 82.8723404,0.531914894 L79.5319149,0.531914894 Z" id="Combined-Shape"></path>
+            </g>
+            <g id="collection-1">
+              <rect id="background" x="0.531914894" y="0.531914894" width="83.9361702" height="94.9361702" rx="2.12765957"></rect>
+              <rect id="window-4" x="47.8125" y="48" width="31.875" height="32" rx="2.12765957"></rect>
+              <rect id="window-3" x="5.3125" y="48" width="31.875" height="32" rx="2.12765957"></rect>
+              <rect id="window-2" x="47.8125" y="5.33333333" width="31.875" height="32" rx="2.12765957"></rect>
+              <rect id="window-1" x="5.3125" y="5.33333333" width="31.875" height="32" rx="2.12765957"></rect>
+            </g>
+          </g>
+        </g>
+      </svg>
+    );
+  }
+}
 
 class IconArrowThickLeft extends React.Component {
   static propTypes = {
@@ -68,11 +107,10 @@ class IconCheckMark extends React.Component {
 
   render() {
     return (
-      <svg width={this.props.size} height={this.props.size} viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
-        <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-          <g id="Artboard-6" fill={this.props.fillColor}>
-            <path d="M14.5810225,21.4535604 C14.7483703,21.4100207 14.90653,21.3223046 15.0377989,21.1910358 L22.6564144,13.5724202 C23.0438935,13.1849411 23.0459704,12.5437672 22.6517873,12.1495842 L21.2508234,10.7486202 C20.8505898,10.3483867 20.2196149,10.3523655 19.8279873,10.7439931 L13.743097,16.8288834 L11.9209299,15.0067163 C11.5266963,14.6124827 10.8914905,14.6085095 10.4958207,15.0041793 L9.50417927,15.9958207 C9.11134683,16.3886532 9.108137,17.0223506 9.50671635,17.4209299 L13.3719633,21.2861769 C13.7003553,21.6145689 14.1959506,21.6721711 14.5810225,21.4535604 Z M16,29 C8.82029825,29 3,23.1797017 3,16 C3,8.82029825 8.82029825,3 16,3 C23.1797017,3 29,8.82029825 29,16 C29,23.1797017 23.1797017,29 16,29 Z" id="Combined-Shape"></path>
-          </g>
+      <svg width={this.props.size} height={this.props.size} viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <g id="icon-check-mark" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+          <path d="M34.515206,64.5123812 L22.8508108,52.8441027 C22.2651825,52.2582891 22.2652575,51.308671 22.8509785,50.7229501 L22.8508108,50.7227824 L29.2147718,44.3588213 C29.8005582,43.7730349 30.7503057,43.7730349 31.3360921,44.3588213 L43.0029419,56.0256711 L90.0226009,9.01169393 C90.6083976,8.42596679 91.5580965,8.42599494 92.1438584,9.01175682 L92.1439213,9.01169393 L98.5078823,15.375655 C99.0936688,15.9614414 99.0936688,16.9111889 98.5078823,17.4969753 L43,73.0048576 L34.5113646,64.5162222 Z" id="check-mark" fill={this.props.fillColor}></path>
+          <path d="M81.8913561,11.4892946 L75.4926012,17.8867855 C68.4939276,12.3235621 59.6350936,9 50,9 C27.3563253,9 9,27.3563253 9,50 C9,72.6436747 27.3563253,91 50,91 C72.6436747,91 91,72.6436747 91,50 C91,44.2947767 89.8347014,38.8617235 87.7291338,33.92587 L94.4918898,27.163114 C98.0126877,34.0086966 100,41.7722675 100,50 C100,77.6142375 77.6142375,100 50,100 C22.3857625,100 0,77.6142375 0,50 C0,22.3857625 22.3857625,0 50,0 C62.12167,0 73.235866,4.31350886 81.8913561,11.4892946 Z" id="circle" fill={this.props.fillColor}></path>
         </g>
       </svg>
     );
@@ -182,6 +220,35 @@ class IconPencilWithSquares extends React.Component {
   }
 }
 
+class IconPlus extends React.Component {
+  static propTypes = {
+    size: PropTypes.string,
+    fillColor: PropTypes.string,
+    strokeColor: PropTypes.string
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      size: "32",
+      fillColor: "#EEEEEE",
+      strokeColor: "#EEEEEE"
+    };
+  }
+
+  render() {
+    return (
+      <svg width={this.props.size} height={this.props.size} viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <g id="icon-plus" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+          <rect id="vertical" fill={this.props.fillColor} x="45" y="0" width="10" height="100"></rect>
+          <rect id="horizontal" fill={this.props.fillColor} x="0" y="45" width="100" height="10"></rect>
+        </g>
+      </svg>
+    );
+  }
+}
+
 class IconX extends React.Component {
   static propTypes = {
     size: PropTypes.string,
@@ -215,6 +282,6 @@ class IconX extends React.Component {
 }
 
 export {
-  IconArrowThickLeft, IconArrowThickRight, IconCheckMark, IconCircle,
-  IconFilledCircle, IconPencilWithSquares, IconX
+  IconCollection, IconArrowThickLeft, IconArrowThickRight, IconCheckMark,
+  IconCircle, IconFilledCircle, IconPencilWithSquares, IconPlus, IconX
 };
