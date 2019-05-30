4e84e4232a3e9a782c4a6dffbedeebb1a6ab6984
Abhishek Chandrasekhar <me@abhchand.me>
2019-05-30 17:40:53 -0400

    Adding Jest and react-testing-library

---
+++ spec/javascript/jest.config.js
@@ -0,0 +1,23 @@
+// See: jestjs.io/docs/configuration.html
+module.exports = {
+  "rootDir": "../../",
+  "roots": [
+    "app/javascript",
+    "spec/javascript"
+  ],
+  "moduleFileExtensions": [
+    "js",
+    "jsx"
+  ],
+  "moduleDirectories": [
+    "node_modules",
+    "app/javascript"
+  ],
+  "setupFilesAfterEnv": [
+    "<rootDir>/spec/javascript/jest.setup.js"
+  ],
+  "transform": {
+    "\\.jsx?$": "babel-jest"
+  },
+  "verbose": false
+};
