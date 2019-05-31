1dc5eaf0043ef352ce2691449c2488c677e0d9fe
Abhishek Chandrasekhar <me@abhchand.me>
2019-05-31 12:30:50 -0400

    Adding action notification specs

---
+++ spec/javascript/components/shared/action_notifications/action_notification.spec.jsx
@@ -0,0 +1,182 @@
+import { cleanup, fireEvent, render } from "react-testing-library";
+import ActionNotification from "components/shared/action_notifications/action_notification";
+import React from "react";
+
+let onClose;
+let notification;
+
+const defaultDuration = ActionNotification.defaultProps.duration;
+const defaultCloseButtonLabel = ActionNotification.defaultProps.closeButtonLabel;
+
+beforeEach(() => {
+  jest.useFakeTimers();
+
+  onClose = jest.fn();
+  notification = { id: "abcde", content: <div className="content">content</div>, type: "error" };
+});
+
+afterEach(cleanup);
+
+describe("<ActionNotification />", () => {
+  it("renders the component", () => {
+    const rendered = renderComponent();
+
+    const actionNotification = rendered.getByTestId("notification-abcde");
+    expect(actionNotification).toHaveClass("notification--error");
+
+    const content = rendered.container.querySelector(".content");
+    expect(content).toHaveTextContent("content");
+
+    const closeButton = rendered.getByTestId("notification-abcde-close-btn");
+    expect(closeButton).toHaveTextContent(defaultCloseButtonLabel);
+  });
+
+  it("closes after the duration expires", () => {
+    renderComponent();
+
+    // 0.0 secs
+    expect(onClose).not.toBeCalled();
+
+    // <defaultDuration> secs
+    jest.advanceTimersByTime(defaultDuration);
+    expect(onClose).toBeCalled();
+    expect(onClose).toHaveBeenCalledTimes(1);
+  });
+
+  it("persists on mouse hover", () => {
+    const now = Date.now();
+    mockDateOnceAs(now);
+
+    const rendered = renderComponent({ duration: 1000 });
+    const actionNotification = rendered.getByTestId("notification-abcde");
+
+    // 0.0 secs
+    expect(onClose).not.toBeCalled();
+
+    // 0.4 secs
+    mockDateOnceAs(now + 400);
+    jest.advanceTimersByTime(400);
+    fireEvent.mouseEnter(actionNotification);
+    expect(onClose).not.toBeCalled();
+
+    // 0.6 secs
+    mockDateOnceAs(now + 200);
+    jest.advanceTimersByTime(200);
+    fireEvent.mouseLeave(actionNotification);
+    expect(onClose).not.toBeCalled();
+
+    // 1.0 secs
+    // Mouse hover lasted 0.2 seconds, which doesn'tt contribute
+    // towards the total persisted duration. So notification should
+    // not yet be closed.
+    mockDateOnceAs(now + 400);
+    jest.advanceTimersByTime(400);
+    expect(onClose).not.toBeCalled();
+
+    // 1.2 secs
+    mockDateOnceAs(now + 200);
+    jest.advanceTimersByTime(200);
+    expect(onClose).toBeCalled();
+    expect(onClose).toHaveBeenCalledTimes(1);
+  });
+
+  describe("notification type", () => {
+    it("defaults to success if notification type is null", () => {
+      notification.type = null;
+
+      const rendered = renderComponent();
+
+      const actionNotification = rendered.getByTestId("notification-abcde");
+      expect(actionNotification).toHaveClass("notification--success");
+    });
+
+    it("defaults to success if notification type is invalid", () => {
+      notification.type = "foo";
+
+      const rendered = renderComponent();
+
+      const actionNotification = rendered.getByTestId("notification-abcde");
+      expect(actionNotification).toHaveClass("notification--success");
+    });
+  });
+
+  describe("content", () => {
+    it("wraps the content in <span> tags if content is a plaintext string", () => {
+      notification.content = "content";
+
+      const rendered = renderComponent();
+
+      const content = rendered.container.querySelector("span");
+      expect(content).toHaveTextContent("content");
+    });
+  });
+
+  describe("isDismissable prop", () => {
+    it("renders a close button when set to true", () => {
+      const rendered = renderComponent({ isDismissable: true });
+
+      const closeButton = rendered.getByTestId("notification-abcde-close-btn");
+      expect(closeButton).toHaveTextContent(defaultCloseButtonLabel);
+    });
+
+    it("does not render a close button when set to false", () => {
+      const rendered = renderComponent({ isDismissable: false });
+
+      const closeButton = rendered.queryByTestId("notification-abcde-close-btn");
+      expect(closeButton).toBeNull();
+    });
+
+    it("allows the notification to be dismissed on click", () => {
+      const rendered = renderComponent();
+      const closeButton = rendered.getByTestId("notification-abcde-close-btn");
+
+      // 0.0 secs
+      expect(onClose).not.toBeCalled();
+
+      // <0.5 * defaultDuration> secs
+      jest.advanceTimersByTime(defaultDuration / 2);
+      fireEvent.click(closeButton);
+      expect(onClose).toBeCalled();
+      expect(onClose).toHaveBeenCalledTimes(1);
+    });
+
+
+    describe("closeButtonLabel prop", () => {
+      it("overrides the default close button label when present", () => {
+        const rendered = renderComponent({ closeButtonLabel: "Foo" });
+
+        const closeButton = rendered.getByTestId("notification-abcde-close-btn");
+        expect(closeButton).toHaveTextContent("Foo");
+      });
+    });
+  });
+
+  describe("duration prop", () => {
+    it("overrides the default duration when present", () => {
+      renderComponent({ duration: defaultDuration + 1000 });
+
+      // 0.0 secs
+      expect(onClose).not.toBeCalled();
+
+      // <defaultDuration> secs
+      jest.advanceTimersByTime(defaultDuration);
+      expect(onClose).not.toBeCalled();
+
+      // <defaultDuration + 1.0> secs
+      jest.advanceTimersByTime(1000);
+      expect(onClose).toBeCalled();
+      expect(onClose).toHaveBeenCalledTimes(1);
+    });
+  });
+});
+
+const mockDateOnceAs = (unixTime) => {
+  jest.spyOn(global.Date, 'now').mockImplementationOnce(() => new Date(unixTime).valueOf());
+};
+
+const renderComponent = (additionalProps = {}) => {
+  const fixedProps = { notification: notification, onClose: onClose };
+  const props = {...fixedProps, ...additionalProps };
+
+  return render(<ActionNotification {...props} />);
+};
---
+++ spec/javascript/components/shared/action_notifications/action_notifications.spec.jsx
@@ -0,0 +1,121 @@
+import { cleanup, render } from "react-testing-library";
+import ActionNotification from "components/shared/action_notifications/action_notification";
+import ActionNotifications from "components/shared/action_notifications/action_notifications";
+import React from "react";
+
+let notifications;
+
+beforeEach(() => {
+  jest.useFakeTimers();
+
+  notifications = [
+    { id: "abcde", content: <div className="content">content</div>, type: "error" },
+    { id: "fghij", content: "other content", type: "success" }
+  ];
+});
+
+afterEach(cleanup);
+
+describe("<ActionNotifications />", () => {
+  it("renders the component", () => {
+    const rendered = renderComponent();
+
+    // Notification 1
+
+    let actionNotification = rendered.getByTestId("notification-abcde");
+    expect(actionNotification).toHaveClass("notification--error");
+
+    let content = rendered.container.querySelector(".content");
+    expect(content).toHaveTextContent("content");
+
+    // Notification 2
+
+    actionNotification = rendered.getByTestId("notification-fghij");
+    expect(actionNotification).toHaveClass("notification--success");
+
+    content = rendered.container.querySelector("span");
+    expect(content).toHaveTextContent("other content");
+  });
+
+  describe(".add", () => {
+    it("adds a notification to the group", () => {
+      const rendered = renderComponent();
+
+      let actionNotifications = rendered.container.querySelectorAll(".notification");
+      expect(actionNotifications).toHaveLength(2);
+
+      const newNotification = { id: "klmno", content: "new content", type: "success" };
+      window.action_notifications.add(newNotification);
+
+      actionNotifications = rendered.container.querySelectorAll(".notification");
+      expect(actionNotifications).toHaveLength(3);
+    });
+
+    it("handles notifications added at different times", () => {
+      const rendered = renderComponent();
+
+      let actionNotifications = rendered.container.querySelectorAll(".notification");
+      expect(actionNotifications).toHaveLength(2);
+
+      const newNotification = { id: "klmno", content: "new content", type: "success" };
+      window.action_notifications.add(newNotification);
+
+      actionNotifications = rendered.container.querySelectorAll(".notification");
+      expect(actionNotifications).toHaveLength(3);
+    });
+  });
+
+  describe("closeButtonLabel prop", () => {
+    it("sets the label on the children when present", () => {
+      const rendered = renderComponent({ closeButtonLabel: "Foo" });
+
+      const closeButton = rendered.getByTestId("notification-abcde-close-btn");
+      expect(closeButton).toHaveTextContent("Foo");
+    });
+  });
+
+  describe("duration prop", () => {
+    it("sets the duration on the children when present", () => {
+      const rendered = renderComponent({ duration: 1500 });
+
+      // 0.0 secs
+      let actionNotifications = rendered.container.querySelectorAll(".notification");
+      expect(actionNotifications).toHaveLength(2);
+
+      // There's an additional 500ms `transitionLeaveTimeout` on the group
+      // during which the component will still be present
+
+      // 1.5 secs
+      jest.advanceTimersByTime(1500);
+      actionNotifications = rendered.container.querySelectorAll(".notification");
+      expect(actionNotifications).toHaveLength(2);
+
+      // 2.0 secs
+      jest.advanceTimersByTime(500);
+      actionNotifications = rendered.container.querySelectorAll(".notification");
+      expect(actionNotifications).toHaveLength(0);
+    });
+  });
+
+  describe("isDismissable prop", () => {
+    it("sets the isDismissable on the children when present", () => {
+      // This test is written with the assumption that isDismissable is
+      // TRUE by default. This is so that we can set `{isDismissable: false }`
+      // on the parent and verify that the props are being set on the children.`
+      const defaultIsDismissable = ActionNotification.defaultProps.isDismissable;
+      expect(defaultIsDismissable).toBeTruthy();
+
+      const rendered = renderComponent({ isDismissable: false });
+
+      const closeButton = rendered.container.querySelector('[data-testid="notification-abcde-close-btn"]');
+      expect(rendered.container).not.toContainElement(closeButton);
+    });
+  });
+});
+
+const renderComponent = (additionalProps = {}) => {
+  const fixedProps = { notifications: notifications };
+  const props = {...fixedProps, ...additionalProps };
+
+  return render(<ActionNotifications {...props} />);
+};
