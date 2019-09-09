b2508748faf4a38c62975bf61a0e1c26f0806857
abhchand <me@abhchand.me>
2019-09-09 19:20:24 -0400

    [Girder] Adding `LoadingIconEllipsis` component for modals

---
+++ app/javascript/components/shared/modal/loading_icon_ellipsis.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LoadingIconEllipsis = () => {
+  return (
+    <div className="modal-content--loading">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default LoadingIconEllipsis;
