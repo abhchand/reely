b2f53f92f0d5f5882e67e628bbc15958b34bc67d
abhchand <me@abhchand.me>
2019-10-31 00:48:40 -0400

    Deleting duplicate util file

--- app/javascript/utils.js
+++
@@ -1,6 +0,0 @@
-function parseKeyCode(event) {
-  // See: https://stackoverflow.com/q/4285627/2490003
-  return (typeof event.which == "number") ? event.which : event.keyCode;
-}
-
-export {parseKeyCode};
