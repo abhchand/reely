7d8cadaa269cec8f587c88eb491a032d504b54ce
abhchand <me@abhchand.me>
2019-12-12 17:55:55 -0500

    [Girder] Disable SSL redirect on production since nginx handles this

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # Disable SSL redirect - nginx handles this on production
+  config.force_ssl = false
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
