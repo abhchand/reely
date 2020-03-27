4fedfba79f9135630b63d36a3041bb3dedab7e26
abhchand <me@abhchand.me>
2020-03-27 17:55:07 -0400

    [Girder] `ApplicationRecord#serialize_errors` should return full error messages

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,15 +4,17 @@ class ApplicationRecord < ActiveRecord::Base
   def serialize_errors
     errors_json = []
 
-    errors.messages.each do |field, messages|
-      messages.each do |message|
-        # NOTE: Errors not visible to end users are not translated
-        errors_json << {
-          title: "Invalid #{field}",
-          description: message,
-          status: "403"
-        }
-      end
+    errors.full_messages.each do |message|
+      # NOTE: Errors not visible to end users are not translated
+      # so we don't translate `:title`
+      # Also, ActiveRecord generates `full_messages` by concatenating
+      # the field name with the message, so the first word should always
+      # be the field name
+      errors_json << {
+        title: "Invalid #{message.split(' ').first}",
+        description: message,
+        status: "403"
+      }
     end
 
     { errors: errors_json, status: "403" }
