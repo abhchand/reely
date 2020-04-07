5f580c5ac87ca380f5e3c9fd2281dc4c5b4d38b3
abhchand <me@abhchand.me>
2020-04-07 15:41:09 -0400

    [Girder] Provider helpers to track start and completion of async processes

---
+++ app/frontend/javascript/utils/async-registration.js
@@ -0,0 +1,29 @@
+function initializeAsyncRegistration() {
+  if (typeof window.asyncRegistration === 'object' && Array.isArray(window.asyncRegistration)) {
+    return;
+  }
+
+  window.asyncRegistration = [];
+}
+
+function registerAsyncProcess(name) {
+  initializeAsyncRegistration();
+
+  if (window.asyncRegistration.indexOf(name) > -1) {
+    return;
+  }
+
+  window.asyncRegistration.push(name);
+}
+
+function unregisterAsyncProcess(name) {
+  const index = window.asyncRegistration.indexOf(name);
+  if (index > -1) {
+    window.asyncRegistration.splice(index, 1);
+  }
+}
+
+export {
+  registerAsyncProcess,
+  unregisterAsyncProcess
+};
--- spec/support/feature_helpers/async_helpers.rb
+++ spec/support/feature_helpers/async_helpers.rb
@@ -17,8 +17,23 @@ module FeatureHelpers
     end
   end
 
+  def wait_for_async_process(name)
+    return unless @javascript_enabled
+
+    Timeout.timeout(Capybara.default_max_wait_time) do
+      loop until finished_async_process?(name)
+    end
+  end
+
   def finished_all_xhr_requests?
     page.evaluate_script("jQuery.active").tap { |result| return result.zero? }
+  rescue Timeout::Error # rubocop:disable Lint/HandleExceptions
+  end
+
+  def finished_async_process?(name)
+    page.evaluate_script("window.asyncRegistration").tap do |result|
+      return !result.include?(name)
+    end
   rescue Timeout::Error # rubocop:disable Lint/HandleExceptions
   end
 end
