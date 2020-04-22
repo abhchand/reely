920603b6ca39e4a21748b54f69a5737c4be54055
abhchand <me@abhchand.me>
2020-04-21 23:32:20 -0400

    [Girder] Adding Accordion Table Helpers

---
+++ spec/support/feature_helpers/accordion_table_helpers.rb
@@ -0,0 +1,42 @@
+module FeatureHelpers
+  def accordion_table_row_for(record)
+    type = record.class.name.underscore
+    page.find(
+      ".accordion-table-row[data-type='#{type}'][data-id='#{record.id}']"
+    )
+  end
+
+  def within_descendants_of(record, &block)
+    type = record.class.name.underscore
+
+    # See: developer.mozilla.org/en-US/docs/Web/CSS/Adjacent_sibling_combinator
+    scope = page.find(
+      ".accordion-table-row[data-type='#{type}'][data-id='#{record.id}'] + "\
+        ".accordion-table-row-descendants"
+    )
+
+    within(scope) { yield }
+  end
+
+  def click_accordion_table_create(type)
+    page.
+      find(".accordion-table--#{type} .admin-rubric-editor__button--create").
+      click
+  end
+
+  def toggle_accordion_row(record)
+    accordion_table_row_for(record).
+      find(".accordion-table-row__expand-arrow").
+      click
+  end
+
+  def expect_accordion_table_rows_to_be(data, type:)
+    actual =
+      page.all(".accordion-table-row[data-type='#{type}']").map do |row|
+        row["data-id"]
+      end
+    expected = data.map(&:id).map(&:to_s)
+
+    expect(actual).to eq(expected)
+  end
+end
