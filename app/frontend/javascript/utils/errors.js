60218498f89b11d1e70f0787d843dedfbf2ace43
abhchand <me@abhchand.me>
2020-07-10 18:36:50 -0400

    [Girder] Handle standard JS TypeError

--- app/frontend/javascript/utils/errors.js
+++ app/frontend/javascript/utils/errors.js
@@ -1,4 +1,7 @@
 function parseJsonApiError(error) {
+  // If we're dealing with a standard JS TypeError
+  if (!error.hasOwnProperty('response')) { return error.message; }
+
   const errorObj = error.response.data.errors[0] || {};
   return errorObj.description || errorObj.title || I18n.t('generic_error');
 }
