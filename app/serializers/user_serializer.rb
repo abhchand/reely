aad5852174853441169df0f888d17919dd5717f0
abhchand <me@abhchand.me>
2020-07-10 11:05:56 -0400

    [Girder] Always return `avatarPath` as an attribute

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,10 +2,20 @@ class UserSerializer < ApplicationSerializer
   set_id :synthetic_id
   attributes :first_name, :last_name, :email, :last_sign_in_at
 
+  attributes :avatar_paths do |user, _params|
+    u = user
+    u = UserPresenter.new(u, view: nil) if user.is_a?(User)
+
+    {}.tap do |paths|
+      ([nil] + User::AVATAR_SIZES.keys).each do |size|
+        paths[size || :original] = u.avatar_path(size: size)
+      end
+    end
+  end
+
   # Below attributes are only available when the `user` is wrapped in
   # a `UserPresenter`
   attributes :roles, if: proc { |user, _params| user.respond_to?(:roles) }
-  attributes :avatar_path, if: proc { |user, _params| user.respond_to?(:avatar_path) }
 
   link :self do |user, _params|
     Rails.application.routes.url_helpers.api_v1_user_url(user)
