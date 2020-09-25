117b66796c23a87446a4b1ad423f6ad6bf9c04cc
abhchand <me@abhchand.me>
2020-09-25 13:10:29 -0400

    [Girder] Handle `undefined` os name

--- app/frontend/javascript/utils/os.js
+++ app/frontend/javascript/utils/os.js
@@ -2,6 +2,9 @@ function os() {
   // See: https://stackoverflow.com/a/9514476/2490003
   const osName = window.navigator.oscpu;
 
+  if (!osName) {
+    return 'other';
+  }
   if (osName.match(/windows/iu)) {
     return 'windows';
   } else if (osName.match(/os x/iu)) {
